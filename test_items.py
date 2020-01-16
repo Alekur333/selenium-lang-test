3697d39c92b99cb5def42e046cb393ed62440dac
ku <alstar369@gmail.com>
2020-01-16 16:29:26 +0500

    eanable time.sleep(3)

--- test_items.py
+++ test_items.py
@@ -5,6 +5,7 @@ def test_add_to_basket_button_exist(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(3)
+
     add_to_basket_button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
     assert add_to_basket_button > 0, "Кнопка 'Добавить в корзину' не найдена"
 
